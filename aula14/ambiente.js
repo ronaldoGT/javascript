d2a2f1c195979b1496e13608688a5169e4b796a5
ronaldoGT <117641834+ronaldoGT@users.noreply.github.com>
2023-01-18 09:01:33 -0300

    aula 14 for

---
+++ aula14/ambiente.js
@@ -0,0 +1,18 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(` Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+for ( var c=1; c<=10; c++) {
+    console.log(` Passo ${c}`) 
+}
+*/
+console.log('Vai começar')
+for ( var c=1; c<=10; c++) {
+    console.log(` Passo ${c}`) 
+}
+console.log('FIM!')
