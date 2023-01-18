2e5f02ec1f7a8ebfc674995b8c0f535e21f0dff5
ronaldoGT <117641834+ronaldoGT@users.noreply.github.com>
2023-01-18 08:37:11 -0300

    aula 13 while / do

---
+++ aula13/ambiente.js
@@ -0,0 +1,34 @@
+//Teste lógico no final
+var c = 1
+do {
+    console.log(` Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+
+
+//Teste lógico no início
+/*
+var c = 1
+while (c <= 6) {
+    console.log(` Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
