3e7e57701b24e840861f7299ee9d1206ba5a3631
ronaldoGT <117641834+ronaldoGT@users.noreply.github.com>
2023-01-21 06:49:54 -0300

    aula 16 funções

---
+++ aula16/funcao02.js
@@ -0,0 +1,18 @@
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+console.log(soma(7, 3)) // resultado = 10
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+console.log(soma(7)) // resultado = NaN
+
+/*function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(7)) // resultado = 7
+*/
